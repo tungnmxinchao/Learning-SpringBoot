4ff7894ba5a0cf47d43a5895e85f0a06dc13cbef
ducmanh <bobolavoalo@gmail.com>
2024-05-29 10:48:56 +0700

    exception handler

--- jpaAndCrud/src/main/java/com/example/jpaandcrud/service/UserService.java
+++ jpaAndCrud/src/main/java/com/example/jpaandcrud/service/UserService.java
@@ -18,6 +18,9 @@ public class UserService {
     public User createUser(UserCreationRequest request){
         User user = new User();
 
+        if(userRepository.existsByUsername(request.getUsername())){
+            throw new RuntimeException("user is existed!");
+        }
         user.setUsername(request.getUsername());
         user.setPassword(request.getPassword());
         user.setFirstName(request.getFirstName());
